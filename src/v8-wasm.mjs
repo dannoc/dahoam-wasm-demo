792ae153cac8d49db21fe6bf39aa13d4fd1d72dc
Mathias Bynens <mathias@qiwi.be>
2018-07-18 13:55:49 +0200

    Create a JS module bundle instead of a classic script bundle

--- src/v8-wasm.mjs
+++ src/v8-wasm.mjs
@@ -3,14 +3,14 @@ import PrintVisitor from './print-visitor.mjs';
 import CompilationVisitor from './compilation-visitor.mjs';
 
 export default function compile({ input, elements }) {
-  const source_ast = esprima.parse(input);
+  const jsAst = esprima.parse(input);
   const printVisitor = new PrintVisitor();
   const compilationVisitor = new CompilationVisitor();
 
-  printVisitor.visit(source_ast);
+  printVisitor.visit(jsAst);
   elements.ast.textContent = printVisitor.getResult();
 
-  compilationVisitor.visit(source_ast);
+  compilationVisitor.visit(jsAst);
   elements.wasm.textContent = compilationVisitor.getSource();
 
   const buffer = compilationVisitor.getBytes();
