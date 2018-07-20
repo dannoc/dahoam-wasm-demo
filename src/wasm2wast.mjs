da4f19c0b8a1e63f29a2a49a572b9e23f0fb6ee5
Mathias Bynens <mathias@qiwi.be>
2018-07-20 10:44:15 +0200

    Remove wasm-parser and wast-printer workarounds 🎉

--- src/wasm2wast.mjs
+++ src/wasm2wast.mjs
@@ -1,8 +1,5 @@
-// These globals are exported by the bundled versions of wasm-parser
-// and wast-printer. See `public/index.html` and `postinstall` in
-// `package.json`.
-const decode = _webassemblyjs_wasmParser.decode;
-const print = _webassemblyjs_wastPrinter.print;
+import { decode } from '@webassemblyjs/wasm-parser';
+import { print } from '@webassemblyjs/wast-printer';
 
 export default function wasm2wast(bytes) {
   const wasmAst = decode(bytes);
